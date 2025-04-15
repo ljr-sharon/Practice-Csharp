72b09ba20c0a651f2b1bb2e92643b35354911b61
ljr-sharon <sn200860325@gmail.com>
2025-04-16 00:32:16 +0800

    反轉字串

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // array.reverse
+            //Console.WriteLine("輸入英文句子：");
+            //string input = Console.ReadLine();
+            //var chars = input.ToCharArray();
+            //Array.Reverse(chars);
+            //Console.WriteLine("反轉後的句子：");
+            //Console.WriteLine(chars);
+
+            // 反轉字串
+            Console.WriteLine("輸入英文句子：");
+            string input =  Console.ReadLine() ;
+            char[] chars = input.ToCharArray();
+
+            for (int i = chars.Length-1; i >= 0; i--)
+            {
+                Console.Write(chars[i]);
+            }
+
+
+        }
+    }
+}
