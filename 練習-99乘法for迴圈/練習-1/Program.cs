8764b4d218429b26aa0e52797bd493372c0fdb9c
ljr-sharon <sn200860325@gmail.com>
2025-04-15 23:26:47 +0800

    99

---
+++ 練習-99乘法for迴圈/練習-1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace 練習_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!\n");
+
+            int a;
+
+            for (int i = 1; i <10; i++)
+            {
+               
+                for (int j = 1; j <10; j++)
+                { 
+                    a= i * j;
+                    Console.Write(j+"*"+i+"="+a );
+
+                    if (j == 9)
+                        Console.Write("\n");
+                    else
+                        Console.Write("\t");
+                    
+                }   
+            }
+            
+           
+
+
+        }
+    }
+}
