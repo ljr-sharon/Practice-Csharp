e64ec78b47312cb36d641fdf1f3cef5a51656ce4
ljr-sharon <sn200860325@gmail.com>
2025-04-15 23:53:59 +0800

    some practice

---
+++ 星字塔/作業-1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace 作業_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxWidth = 9;                     // 最大寬度
+            int totalLines = (maxWidth + 1) / 2;    // 根據星號數量，計算總列數
+
+            for (int row = 0; row < totalLines; row++)
+            {
+                int stars = row * 2 + 1;                // 每列星號數量：依序 1, 3, 5, ...
+                int spaces = totalLines - row - 1;        // 每列前置空白數量：使星號置中
+
+                Console.WriteLine(new string(' ', spaces) + new string('*', stars));
+            }
+            Console.WriteLine("----------------------");
+            for (int i = 9; i >= 0; i -= 2)
+            {
+                Console.WriteLine(new string(' ', (9 - i) / 2) + new string('*', i));
+            }
+        }
+    }
+}
--- 練習-99乘法for迴圈/練習-1/Program.cs
+++ 練習-99乘法for迴圈/練習-1/Program.cs
@@ -4,28 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!\n");
-
             int a;
 
-            for (int i = 1; i <10; i++)
+            for (int i = 1; i < 10; i++)
             {
-               
-                for (int j = 1; j <10; j++)
-                { 
-                    a= i * j;
-                    Console.Write(j+"*"+i+"="+a );
+
+                for (int j = 1; j < 10; j++)
+                {
+                    a = i * j;
+                    Console.Write(j + "*" + i + "=" + a);
 
                     if (j == 9)
+                    {
                         Console.Write("\n");
+                    }
                     else
+                    {
                         Console.Write("\t");
-                    
-                }   
-            }
-            
-           
+                    }
 
+                }
+            }
 
         }
     }
