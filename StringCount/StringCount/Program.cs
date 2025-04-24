9f23eb6d4943b93c2851bae5427c9833c9c9f4c6
ljr-sharon <sn200860325@gmail.com>
2025-04-24 16:43:13 +0800

    print Repeat String
    印出重複字母的次數

---
+++ StringCount/StringCount/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace StringCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.Replace(" ", "");
+            char[] chars = input.ToCharArray();
+            Array.Sort(chars);
+            int count = 1;
+            int currentIndex = 0;
+            bool hasdouble = false;
+
+            for (int i = currentIndex+1; i < chars.Length; i++)
+            {
+                if (chars[i] == chars[currentIndex])
+                {
+                    count++;
+                    hasdouble = true;
+                }
+                else
+                {
+                    if (count > 1)
+                    {
+                        Console.WriteLine($"The character {chars[currentIndex]} appears {count} times.");
+                    }
+                    currentIndex = i;
+                    count = 1;
+                }
+            }
+            if (count > 1)
+            {
+                Console.WriteLine($"The character {chars[currentIndex]} appears {count} times.");
+            }
+            else if(!hasdouble)
+            {
+                Console.WriteLine("no reoccurrence");
+            }
+
+        }
+    }
+}
