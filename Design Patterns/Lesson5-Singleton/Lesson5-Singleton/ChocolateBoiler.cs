674062b30de1d2be726c2f285bb36e20b2399b96
ljr-sharon <sn200860325@gmail.com>
2025-07-15 02:36:50 +0800

    Lesson5-Singleton

---
+++ Design Patterns/Lesson5-Singleton/Lesson5-Singleton/ChocolateBoiler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_Singleton
+{
+    public sealed class ChocolateBoiler // 防止被繼承
+    {
+        private bool empty;
+        private bool boiled;
+        private static volatile ChocolateBoiler _instance;
+        // 專門用來鎖的靜態物件
+        private static readonly object _syncRoot = new object();
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new ChocolateBoiler();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        //private static readonly Lazy<ChocolateBoiler> _lazyInstance
+        //    = new Lazy<ChocolateBoiler>(() => new ChocolateBoiler(),isThreadSafe: true);
+        //public static ChocolateBoiler Instance => _lazyInstance.Value;
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                empty = false;
+                boiled = false;
+            }
+        }
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {
+                empty = true;
+            }
+        }
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                boiled = true;
+            }
+        }
+        public bool IsEmpty()
+        {
+            return empty;
+        }
+        public bool IsBoiled()
+        {
+            return boiled;
+        }
+    }
+}
---
+++ Design Patterns/Lesson5-Singleton/Lesson5-Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson5_Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
